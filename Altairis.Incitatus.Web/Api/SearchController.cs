98f0e86fe3c8ae45b304033888ee6352a07af881
Michal Altair Valášek <github@rider.cz>
2023-05-08 14:07:27 +0200

    Add endpoint to return results as HTML fragment.

--- Altairis.Incitatus.Web/Api/SearchController.cs
+++ Altairis.Incitatus.Web/Api/SearchController.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Mime;
+﻿using System.Globalization;
+using System.Net.Mime;
+using System.Text;
 using Altairis.Services.DateProvider;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -41,6 +43,39 @@ public class SearchController : ControllerBase {
     }
 
     public record SearchResultModel(string Url, string Title, string Description, DateTime? LastUpdated);
+
+    /// <summary>
+    /// Gets search results for specified site as HTML fragment.
+    /// </summary>
+    /// <param name="siteId">The site identifier.</param>
+    /// <param name="query">The search query.</param>
+    /// <param name="locale">The locale code, ie. <c>en-US</c> or <c>cs-CZ</c>. If missing or invalid, server culture is used.</param>
+    /// <param name="dateFormat">The last update date format string in .NET format. If missing, default date format for selected culture is used.</param>
+    /// <returns></returns>
+    [HttpGet("{siteId:guid}.html")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK, "text/html")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> GetHtml(Guid siteId, string query, string? locale = null, string dateFormat = "d") {
+        try {
+            var result = await this.dc.SearchPages(siteId, query).Select(x => new SearchResultModel(x.Url, x.Title, x.Description, x.DateLastUpdated)).ToListAsync();
+            if(!result.Any()) return this.NotFound();
+            var culture = string.IsNullOrEmpty(locale) ? CultureInfo.CurrentCulture : new CultureInfo(locale);
+            var sb = new StringBuilder();
+            foreach (var item in result) {
+                sb.AppendLine($"""
+                    <article>
+                        <header><a href="{item.Url}">{item.Title}</a></header>
+                        <aside><time datetime="{item.LastUpdated:s}">{item.LastUpdated?.ToString(dateFormat, culture)}</time></aside>
+                        <div>{item.Description}</div>
+                    </article>
+                    """);
+            }
+            return this.Content(sb.ToString(), "text/html");
+        } catch (SqlException sex) when (sex.Number == 7645) { // Null or empty full-text predicate.
+            return this.BadRequest();
+        }
+    }
 
     /// <summary>
     /// Marks the specified site as in need of update.
